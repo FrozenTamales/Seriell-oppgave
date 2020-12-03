1c22a18d9958dab0e21e70efcab4a1b13d8ac4a4
unknown <FrozenTamales>
2020-12-03 14:55:31 +0100

    Versjon 1.3.1
    -Noen få endringer i både arduino og python koden i oppg 3, men funker ikke fortsatt ikke

--- SeriellOppg3/Seriell-oppgave3-Python/Seriell_oppgave3.py
+++ SeriellOppg3/Seriell-oppgave3-Python/Seriell_oppgave3.py
@@ -7,11 +7,12 @@ ser = serial.Serial(
 )
 
 while True:
+    ts = time.gmtime()
     try:
         s = str(ser.readline(100).decode())
-        if s != "":
-            f = open("Adgangslogg.txt", "a+")
-            f.write(s)
+        #if s != "": 
+        #    f = open("Adgangslogg.txt", "a+")
+            #f.write(time.strftime("%m-%d %H:%M:%S %Z", ts), s, "\n")
     except:
        print('ERROR')
 time.sleep(1)
--- SeriellOppg3/Seriell_-_oppgave3-Arduino/Seriell_-_oppgave3-Arduino.ino
+++ SeriellOppg3/Seriell_-_oppgave3-Arduino/Seriell_-_oppgave3-Arduino.ino
@@ -1,15 +1,15 @@
 int knapp = 0;
-int potMeter = 0;
+int pot = 0;
 
 void setup() {
   pinMode(7, INPUT);
-
   Serial.begin(2000000);
 }
 
 void loop() {
   knapp = digitalRead(7);
+  pot = analogRead(A0);
 
-  Serial.print(knapp);
+  Serial.write(pot);
   delay(1000);
 }
