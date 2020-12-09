64ff92b3f6712d51d3a73bc341ed334d2946d8b7
unknown <FrozenTamales>
2020-12-09 12:42:34 +0100

    Versjon 1.4
    -Lagt til to oversiktsbilde av Arduino-koblingen
    -Fikset både Arduino koden og Python koden
    -Oppgave 3 funker endelig

--- SeriellOppg3/Seriell-oppgave3-Python/Seriell_oppgave3.py
+++ SeriellOppg3/Seriell-oppgave3-Python/Seriell_oppgave3.py
@@ -2,18 +2,26 @@ import serial
 import time
 from datetime import datetime
 
+
 ser = serial.Serial(
-    'COM4', 2000000, timeout=0, parity=serial.PARITY_NONE, rtscts=1
+    'COM4', 9600, timeout=0, parity=serial.PARITY_NONE, rtscts=1
 )
+
 
 while True:
     ts = time.gmtime()
-    try:
-        s = str(ser.readline(100).decode())
-        #if s != "": 
-        #    f = open("Adgangslogg.txt", "a+")
-            #f.write(time.strftime("%m-%d %H:%M:%S %Z", ts), s, "\n")
-    except:
-       print('ERROR')
-time.sleep(1)
+    print(time.strftime("%m-%d %H:%M:%S %Z", ts))
+    s = str(ser.readline(100).decode())
+
+
+    if s != "":
+        f = open("Adgangslogg.txt", "a+")   
+        f.write(time.strftime("%m-%d %H:%M:%S %Z", ts))
+        f.write(": ")
+        f.write(s)
+        f.write("\n")
+        f.write("\n")
+
+
+    time.sleep(1)
 
--- SeriellOppg3/Seriell_-_oppgave3-Arduino/Seriell_-_oppgave3-Arduino.ino
+++ SeriellOppg3/Seriell_-_oppgave3-Arduino/Seriell_-_oppgave3-Arduino.ino
@@ -1,15 +1,18 @@
 int knapp = 0;
-int pot = 0;
 
 void setup() {
   pinMode(7, INPUT);
-  Serial.begin(2000000);
+  Serial.begin(9600);
 }
 
 void loop() {
   knapp = digitalRead(7);
-  pot = analogRead(A0);
 
-  Serial.write(pot);
+  if (knapp == HIGH) {
+    Serial.write("Knapp aktivert!");
+  }
+  else {
+    Serial.write("");
+  }
   delay(1000);
 }
